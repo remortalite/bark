20803ea4e34639cd77d26d0cb774c4c0b3f650ac
Rem M. <morrig407@gmail.com>
2024-09-02 01:06:13 +0700

    Add table creation

--- db.py
+++ db.py
@@ -20,3 +20,15 @@ class DatabaseManager:
             curs.execute(statement, values or [])
             return curs
 
+    def create_table(self, table_name:str, columns_dict:dict):
+        '''Execute table creation with column type at columns_dict
+
+        Arguments:
+        table_name -- name of the table
+        columns_dict -- dict with keys as columns names and values as data type, e.g. {'id': 'INTEGER PRIMARY KEY'}
+        '''
+        columns_str_list = [f'{k} {v}' for k, v in columns_dict.items()]
+        values_str = ', '.join(columns_str_list)
+        statement = f'CREATE TABLE IF NOT EXISTS {table_name} ({values_str});'
+        self._execute(statement)
+
