7d5118552a452fb84956a0efaa86f0a6213f8028
Rem M. <morrig407@gmail.com>
2024-09-02 00:18:38 +0700

    Add execute to db manager class

--- db.py
+++ db.py
@@ -2,9 +2,21 @@ import sqlite3
 
 
 class DatabaseManager:
-    def __init__(self, db_filename):
+    def __init__(self, db_filename:str):
         self.connection = sqlite3.connect(db_filename)
 
     def __del__(self):
         self.connection.close()
+
+    def _execute(self, statement:str, values:list|tuple=None):
+        '''Execute statement on connection
+
+        Arguments:
+        statement -- contains string, may content placeholders, e.g. "(?, ?)"
+        values -- data to replace placeholders in statement (default: None)
+        '''
+        with self.connection:
+            curs = self.connection.cursor()
+            curs.execute(statement)
+            return curs
 
