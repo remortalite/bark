42a1a7ae8948c160593da6e81d89e1fbc8e97c0d
Rem M. <morrig407@gmail.com>
2024-09-06 13:22:30 +0700

    Add select method to db manager

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Union
 
 
 class DatabaseManager:
@@ -66,3 +67,26 @@ class DatabaseManager:
         delete_criteria = ' AND '.join(placeholder)
         statement = f'DELETE FROM {table_name} WHERE {delete_criteria}'
         self._execute(statement, tuple(criteria.values()))
+
+    def select(self, table_name: str,
+               criteria: Union[dict, None] = None,
+               order_by: Union[list, None] = None) -> sqlite3.Cursor:
+        '''Select data from table with optional criteria and ordering
+
+        Arguments:
+        table_name -- name of the table
+        criteria -- (optional) a dict with parameters of the record to
+                    fetch, i.e. WHERE clause of SQL (e.g. {'id': 1})
+        order_by -- (optional) a list with column names for ordering
+                    to use, i.e. ORDER BY clause (e.g. ['id'])
+        '''
+        criteria = criteria or {}
+        query = f"SELECT * FROM {table_name}"
+        if criteria:
+            placeholder_criteria = [f"{column} = ?" for column in criteria.keys()]
+            select_criteria = ' AND '.join(placeholder_criteria)
+            query += f' WHERE {select_criteria}'
+        if order_by:
+            ordering = ', '.join(order_by or [])
+            query += f' ORDER BY {ordering}'
+        return self._execute(query, tuple(criteria.values()))
