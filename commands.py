52483ad4db5eb30306b4358ea59b4af920b230f6
Rem M. <morrig407@gmail.com>
2024-09-16 17:07:02 +0700

    Add several commands

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
 from db import DatabaseManager
+import sys
 
 
 db = DatabaseManager('bookmarks.db')
@@ -19,6 +20,53 @@ class CreateBookmarksTableCommand:
 
 class AddBookmarkCommand:
     def execute(self, data):
+        '''
+        Add bookmark to db
+
+        Arguments:
+        data -- (required) record data
+
+        Returns
+        success message
+        '''
         data['date_added'] = datetime.utcnow().isoformat()
         db.add('bookmarks', data)
         return 'Bookmark added!'
+
+
+class ListBookmarksCommand:
+    def __init__(self, order_by: str = 'date_added'):
+        self.order_by = order_by
+
+    def execute(self) -> list:
+        '''
+        Execute `select` command from db manager
+
+        Arguments:
+        table_name -- name of the table
+        order_by -- name of the column to sort by (default: 'date_added')
+
+        Returns:
+        list of rows of data
+        '''
+        return db.select('bookmarks', order_by=self.order_by).fetchall()
+
+
+class DeleteBookmars:
+    def execute(self, data: str):
+        '''
+        Delete bookmark from db
+
+        Arguments:
+        data -- (required) record id
+
+        Returns
+        success message
+        '''
+        db.delete('bookmarks', {'id': data})
+        return 'Bookmark deleted!'
+
+
+class QuitCommand:
+    def execute(self):
+        sys.exit()
